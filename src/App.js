578ae748d6cf9505e51f5c7bf5d8d901fe04dd8b
David B <144248533+ccDavidbla@users.noreply.github.com>
2024-03-25 15:56:03 -0400

    Primer commit de tareas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./styles.css";
 
-function App() {
+const App = () => {
+  const [tareas, setTareas] = useState([]);
+
+  useEffect(() => {
+    const tareasAlmacenadas = localStorage.getItem("tareas");
+    if (tareasAlmacenadas) {
+      setTareas(JSON.parse(tareasAlmacenadas));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tareas", JSON.stringify(tareas));
+  }, [tareas]);
+
+  const handleAgregarTarea = (texto) => {
+    if (!texto) return;
+    const nuevaTarea = {
+      texto,
+      completado: false,
+    };
+    const nuevasTareas = [...tareas, nuevaTarea];
+    setTareas(nuevasTareas);
+  };
+
+  const handleEliminarTarea = (indice) => {
+    const nuevasTareas = tareas.filter((tarea, i) => i !== indice);
+    setTareas(nuevasTareas);
+  };
+
+  const handleMarcarCompletada = (indice) => {
+    const nuevasTareas = [...tareas];
+    nuevasTareas[indice].completado = !nuevasTareas[indice].completado;
+    setTareas(nuevasTareas);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Lista de tareas</h1>
+      <ul className="lista-tareas">
+        {tareas.map((tarea, indice) => (
+          <li key={indice} className={tarea.completado ? "tachado" : ""}>
+            <input
+              type="checkbox"
+              checked={tarea.texto}
+              onChange={() => handleMarcarCompletada(indice)}
+            />
+            {tarea.texto}
+            <button onClick={() => handleEliminarTarea(indice)}>Eliminar</button>
+          </li>
+        ))}
+      </ul>
+      <div className="nueva-tarea">
+        <input
+          type="text"
+          placeholder="Agregar nueva tarea"
+          onKeyPress={(event) => {
+            if (event.key === "Enter") {
+              handleAgregarTarea(event.target.value);
+              event.target.value = "";
+            }
+          }}
+        />
+        <button onClick={(e) => handleAgregarTarea(e.target.value)}>Agregar</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
